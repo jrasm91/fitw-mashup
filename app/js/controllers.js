6576c3349adde88853f30588176c24f9a021f9b0
Jason Rasmussen <JRASM91@gmail.com>
2014-04-09 17:06:33 -0600

    update

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -6,7 +6,6 @@ var cs462Controllers = angular.module('cs462Controllers', []);
 cs462Controllers.controller('MainCtrl', ['$scope', 'User','$cookies',
 	function($scope, User, $cookies) {
  		$scope.current = User.get({id : $cookies.user_id});
-
         console.log("Current = ", $scope.current);
 	}
 ])
